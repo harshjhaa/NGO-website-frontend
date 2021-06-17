db563a8ec89a7cda57597871477f6e9784e2fe9c
unknown <harshjhaa11@gmail.com>
2021-06-17 18:16:56 +0530

    Changes in Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,15 +8,15 @@ const Home = (props) => {
       <h2>Images Side by Side</h2>
       <p>Float images side by side:</p>
       <div class="clearfix">
-        {/* <div class="img-container">
-          <img src="img_1.jpg" alt="abcd" style="width:100%" />
+        <div class="img-container">
+          {/* <img src="img_1.jpg" alt="abcd" style="width:100%" /> */}
         </div>
-       /* <div class="img-container">
-          <img src="img_2.jpg" alt="qqdde"style="width:100%" />
+       <div class="img-container">
+          {/* <img src="img_2.jpg" alt="qqdde"style="width:100%" /> */}
         </div>
         <div class="img-container">
-          <img src="img_3.jpg" alt="NGo" style="width:100%" />
-        </div> */}
+          {/* <img src="img_3.jpg" alt="NGo" style="width:100%" /> */}
+        </div>
       </div>
     </div>
   );
